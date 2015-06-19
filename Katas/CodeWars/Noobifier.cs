54b8b6337b4d5653764ad5cd9cff19e39f471752
Susanne Moog <typo3@susannemoog.de>
2015-06-19 22:40:03 +0200

    Noobifier

---
+++ Katas/CodeWars/Noobifier.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas.CodeWars
+{
+    public class Noobifier
+    {
+        public static string N00bify(string text)
+        {
+            string noobString = PrependLol(text);
+            noobString = DoReplacements(noobString);
+            noobString = PrependOmg(text, noobString);
+            noobString = HandleUpperCaseTransformations(text, noobString);
+            noobString = HandleQuestionAndExclamationMarks(noobString);
+            return noobString;
+        }
+
+        private static string HandleQuestionAndExclamationMarks(string noobString)
+        {
+            if (noobString.Contains("?") || noobString.Contains("!"))
+            {
+                string question = "?";
+                string exclamation = "!";
+                int count = noobString.Count(char.IsWhiteSpace);
+                for (int i = 0; i < count; i++)
+                {
+                    if (i%2 == 0)
+                    {
+                        exclamation += "1";
+                    }
+                    else
+                    {
+                        exclamation += "!";
+                    }
+                    question += "?";
+                }
+                noobString = noobString.Replace("?", question).Replace("!", exclamation);
+            }
+            return noobString;
+        }
+
+        private static string HandleUpperCaseTransformations(string text, string noobString)
+        {
+            noobString = ShouldBeFullyUpperCased(text) ? noobString.ToUpper() : EverySecondWordToUpperCase(noobString);
+            return noobString;
+        }
+
+        private static bool ShouldBeFullyUpperCased(string text)
+        {
+            return text.ToLower().StartsWith("h");
+        }
+
+        private static string EverySecondWordToUpperCase(string noobString)
+        {
+            string[] strings = noobString.Split(' ');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i%2 != 0)
+                {
+                    strings[i] = strings[i].ToUpper();
+                }
+            }
+            return string.Join(" ", strings);
+        }
+
+        private static string PrependOmg(string text, string noobString)
+        {
+            if (noobString.Length > 32)
+            {
+                noobString = noobString.Insert(ShouldPrependLol(text) ? 4 : 0, "OMG ");
+            }
+            return noobString;
+        }
+
+        private static string PrependLol(string text)
+        {
+            if (ShouldPrependLol(text))
+            {
+                text = text.Insert(0, "LOL ");
+            }
+            return text;
+        }
+
+        private static bool ShouldPrependLol(string text)
+        {
+            return text.ToLower().StartsWith("w");
+        }
+
+        private static string DoReplacements(string s)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                {"too", "2"},
+                {"to", "2"},
+                {"fore", "4"},
+                {"for", "4"},
+                {"be", "b"},
+                {"Be", "B"},
+                {"are", "r"},
+                {"you", "u"},
+                {"please", "plz"},
+                {"people", "ppl"},
+                {"really", "rly"},
+                {"have", "haz"},
+                {"know", "no"},
+                {"s", "z"},
+                {"oo", "00"},
+                {"\\.", ""},
+                {"\\,", ""},
+                {"\\'", ""}
+            };
+            foreach (KeyValuePair<string, string> keyValuePair in dictionary)
+            {
+                string pattern = keyValuePair.Key;
+                Regex regex = new Regex(@pattern, RegexOptions.IgnoreCase);
+                s = regex.Replace(s, keyValuePair.Value);
+            }
+            return s;
+        }
+    }
+}
---
+++ KatasTests/CodeWars/NoobifierTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Katas.CodeWars;
+namespace KatasTests.CodeWars
+{
+    public class NoobifierTests
+    {
+  
+        [TestCase("too to today", "2 2 2day")]
+        [TestCase("for fore forward", "4 4 4ward")]
+        [TestCase("be are you please people really have know", "b R u PLZ ppl RLY haz NO")]
+        [TestCase("ss", "zz")]
+        [TestCase("with a w", "LOL WITH a W")]
+        [TestCase("0123456789112345678901234567890123456", "OMG 0123456789112345678901234567890123456", TestName = "OMG at more than 32 chars")]
+        [TestCase("w0123456789112345678901234567890123456", "LOL OMG w0123456789112345678901234567890123456", TestName = "OMG at more than 32 chars with LOL")]
+        [TestCase("W0123456789112345678901234567890123456", "LOL OMG W0123456789112345678901234567890123456", TestName = "OMG at more than 32 chars with LOL")]
+        [TestCase("foo", "f00")]
+        [TestCase("fOO", "f00")]
+        [TestCase("bar bar bar bar", "bar BAR bar BAR")]
+        [TestCase("help I need somebody", "HELP I NEED ZOMEBODY")]
+        [TestCase("Help I need somebody", "HELP I NEED ZOMEBODY")]
+        [TestCase(",'.", "")]
+        [TestCase("me me me?", "me ME me???")]
+        [TestCase("me me me? me", "me ME me???? ME")]
+        [TestCase("me me me me!", "me ME me ME!1!1")]
+        [TestCase("me me me me! me", "me ME me ME!1!1! me")]
+        [TestCase("After conversions this should be!", "After CONVERZIONZ thiz ZHOULD b!1!1!")]
+        [TestCase(
+            "Before you know it... wait, what does this have to do with UNO!?",
+            "OMG B4 u NO it WAIT what DOEZ thiz HAZ 2 DO with UNO!1!1!1!1!1!1!1??????????????"
+            )]
+        public void CheckNoobifier(string input, string expected)
+        {
+             Assert.AreEqual(expected, Noobifier.N00bify(input));
+        }
+
+
+
+        [Test]
+        public static void HowRU()
+        {
+            Assert.AreEqual(
+                "HI HOW R U 2DAY?????", 
+                Noobifier.N00bify("Hi, how are you today?")
+            );
+        }
+
+        [Test]
+        public static void LongSentence()
+        {
+            Assert.AreEqual("OMG I think IT would B nice IF we COULD all GET along",
+               Noobifier.N00bify("I think it would be nice if we could all get along."));
+        }
+
+        [Test]
+        public static void CommasMatter()
+        {
+            Assert.AreEqual("Letz EAT Grandma!1!",
+            Noobifier.N00bify("Let's eat, Grandma!"));
+        }
+    }
+}
